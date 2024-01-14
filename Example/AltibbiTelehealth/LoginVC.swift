b66f20bb7abf3c27d8ad57a57d2a6ec2610ca597
shtayyat <mahmoud@altibbi.com>
2024-01-14 10:31:56 +0300

    fix minimum IOS target

--- Example/AltibbiTelehealth/LoginVC.swift
+++ Example/AltibbiTelehealth/LoginVC.swift
@@ -13,7 +13,7 @@ class LoginVC: UIViewController {
 
     @IBOutlet var tokenField: UITextField!
     @IBOutlet weak var errorLbl: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -22,7 +22,7 @@ class LoginVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     @IBAction func loginPressed(_ sender: UIButton) {
         if let tokenTxt = tokenField.text {
             let tokenLength = tokenTxt.count
@@ -30,7 +30,7 @@ class LoginVC: UIViewController {
                 errorLbl.text = "Please Insert Token!"
                 return
             }
-            AltibbiService.initService(token: tokenTxt, baseUrl: "tawuniya.altibb.com", language: "en")
+            AltibbiService.initService(token: tokenTxt, baseUrl: "", language: "en")
             DispatchQueue.main.async {
                 self.performSegue(withIdentifier: "toOptionsSegue", sender: nil)
             }
