2bc7212a6e45ab13ad74a23257940c188bc065b8
Shtayyat <shtayyat.mahmoud@gmail.com>
2025-12-21 19:01:19 +0300

    swift

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "AltibbiTelehealth",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "AltibbiTelehealth",
+            targets: ["AltibbiTelehealth"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/pusher/pusher-websocket-swift.git",
+            from: "10.1.6"
+        ),
+        .package(
+            url: "https://github.com/sendbird/sendbird-chat-sdk-ios.git",
+            from: "4.19.2"
+        )
+    ],
+    targets: [
+        .target(
+            name: "AltibbiTelehealth",
+            dependencies: [
+                .product(name: "PusherSwift", package: "pusher-websocket-swift"),
+                .product(name: "SendbirdChatSDK", package: "sendbird-chat-sdk-ios")
+            ],
+            path: "AltibbiTelehealth/Classes"
+        )
+    ]
+)
+
+
